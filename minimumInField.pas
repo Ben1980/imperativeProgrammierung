283d3fd733da55ef7e0d059803e9915bff9be4c6
Benjamin Mahr <ben.mahr@gmail.com>
2022-02-11 21:03:15 +0100

    min position added

--- minimumInField.pas
+++ minimumInField.pas
@@ -1,4 +1,4 @@
-Program minimumInField (input, output);
+Program minimumInField (input, output); 
 
 	const
 	FIELDSIZE = 20;
@@ -11,16 +11,21 @@ Program minimumInField (input, output);
 	Field : tField;
 	Minimum : integer;
 	i : tIndex;
+	MinPos : tIndex;
 
 begin
 	writeln('Geben Sie ', FIELDSIZE:2, ' Werte ein:');
 	for i := 1 to FIELDSIZE do
 		readln(Field[i]);
 		
+	MinPos := 1;
 	Minimum := Field[1];
 	for i := 2 to FIELDSIZE do
 		if Field[i] < Minimum then
+		begin
 			Minimum := Field[i];
+			MinPos := i;
+		end;
 			
-	writeln('Die kleinste Zahl ist ', Minimum)
+	writeln('Die kleinste Zahl ist ', Minimum, ' und an Position ', MinPos, '.')
 end.
