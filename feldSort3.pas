3e7418bf0856b002ea1be21b35596e9d8526abf1
Benjamin Mahr <ben.mahr@gmail.com>
2022-02-14 15:27:29 +0100

    Update feldSort3.pas

--- feldSort3.pas
+++ feldSort3.pas
@@ -1,1 +1,38 @@
+program FeldSort3 (input output);
 
+    const
+    FELDGROESSE = 5;
+    
+    type
+    tIndex = 1..FELDGROESSE;
+    tFeld = array [tIndex] of integer;
+    
+    var
+    EingabeFeld : tFeld;
+    idx : tIndex;
+    
+    procedure FeldSortieren (var SortFeld : tFeld);
+        var
+        MinPos,
+        i : tIndex;
+        
+        function FeldMinimumPos (Feld : tFeld;
+                                von, bis : tIndex) : tIndex;
+            var
+            MinimumPos,
+            j : tIndex;
+        begin
+        end;
+        
+        procedure vertuschen (var hin, her : integer);
+            var
+            Tausch: integer;
+        begin
+        end;
+    
+    begin
+    end;
+
+begin
+
+end.
