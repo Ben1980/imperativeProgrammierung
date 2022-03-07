8d29660f193a648da9cc43e6e4159bf6ae4896cb
Benjamin Mahr <ben.mahr@gmail.com>
2022-03-07 20:06:40 +0100

    Update aufgabe2_3.pas

--- aufgabe2_3.pas
+++ aufgabe2_3.pas
@@ -1,1 +1,33 @@
+program NaeherungWurzel2 (input, output);
+{ Berechnet die näherung n-ten grades zu Wurzel(2) }
 
+    type
+    tBruch = record
+                Zaehler : Double;
+                Nenner : Double;
+            end;
+            
+    var
+    Grad : integer;
+    NaherungsBruch : tBruch;
+
+    function NaehereWurzel2(inGrad : integer) : tBruch;
+    { Berechnet den n-ten Grad der naeherung zu Wurzel(2) }
+        var
+        Naeherung : tBruch;
+        
+    begin
+        Naeherung.Zaehler := 0;
+        Naeherung.Nenner := 0;
+    
+        NaehereWurzel2 := Naeherung;
+    end;
+    
+begin
+    write('Gib den Grad der Näherung zu Wurzel(2) ein: ');
+    readln(Grad);
+    
+    NaherungsBruch := NaehereWurzel2 (Grad);
+    
+    write('Die Näherung zu Wurzel(2) mit dem Grad ', Grad, ' ist (', NaherungsBruch.Nenner, ', ', NaherungsBruch.Zaehler, ') -> ', NaherungsBruch.Zaehler / NaherungsBruch.Nenner);
+end.
