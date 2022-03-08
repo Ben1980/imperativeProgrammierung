66c7d3fa7223c57e233038afac878e80baefa3c7
Benjamin Mahr <ben.mahr@gmail.com>
2022-03-08 20:52:24 +0100

    Update aufgabe3_1.pas

--- aufgabe3_1.pas
+++ aufgabe3_1.pas
@@ -11,7 +11,27 @@ program WertePolynomAus (input, output);
             
     var
     polynom : tRefPolynom;
+    ergebniss : integer;
+
+    procedure Anfuegen (var ioPolynom : tRefPolynom;
+                        inKoeffizient : integer;
+                        inGrad: tZahl);
+    { Fügt ein Monom an ein Polynom an }
+    begin
+    end;
+    
+    function PolynomAuswerten (var ioPolynom : tRefPolynom;
+                            inI : integer) : integer;
+    { Wertet das Polynom mit x=i aus }
+    begin
+    end;
 
 begin
-
+    New (polynom);
+    Anfuegen(polynom, 17, 0);
+    Anfuegen(polynom, 7, 5);
+    Anfuegen(polynom, 3, 2);
+    
+    ergebniss := PolynomAuswerten(polynom, 2);
+    writeln('Das Ergebniss des Polynoms ist: ', ergebniss);
 end.
