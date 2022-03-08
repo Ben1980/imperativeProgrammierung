db23da96644c4b4436948ac0cc11b734c3cf908b
Benjamin Mahr <ben.mahr@gmail.com>
2022-03-08 20:23:53 +0100

    Update aufgabe3_1.pas

--- aufgabe3_1.pas
+++ aufgabe3_1.pas
@@ -1,5 +1,17 @@
 program WertePolynomAus (input, output);
 { Wertet ein Polynom aus das aus einer verketteten Liste von Monomen besteht }
+    type
+    tZahl = 0..maxint;
+    tRefPolynom = ^tPolynom;
+    tPolynom = record
+                koeffizient: integer;
+                grad: tZahl;
+                next: tRefPolynom;
+            end;
+            
+    var
+    polynom : tRefPolynom;
+
 begin
 
 end.
