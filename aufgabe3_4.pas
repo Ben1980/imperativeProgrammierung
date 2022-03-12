7b6dfe64891cbf4801ff5cf260ff648856a53933
Benjamin Mahr <ben.mahr@gmail.com>
2022-03-12 20:07:12 +0100

    Update aufgabe3_4.pas

--- aufgabe3_4.pas
+++ aufgabe3_4.pas
@@ -1,1 +1,19 @@
+program aufgabe3_4 (input, output);
+{ Füge einer nach indizes geordneten Liste ien weiteres element hinzu }
+	type
+	tRefIndexListe = ^tIndexListe;
+	tIndexListe = record
+					index : integer;
+					wort : string;
+					next : tRefIndexListe;
+				end;
+				
+	procedure einfuegen(var liste : tRefIndexListe;
+						index : integer;
+						wort : string);
+	begin
+	end;
 
+begin
+
+end.
